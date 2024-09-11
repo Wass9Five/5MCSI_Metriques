82c274ed3e659932ffa01b9a23eb88dead0585d7
Wass9Five <wouldlazazi@gmail.com>
2024-09-11 11:27:51 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,17 +1,31 @@
-from flask import Flask, render_template, jsonify
-from urllib.request import urlopen
+from flask import Flask, render_template_string, render_template, jsonify
+from flask import render_template
+from flask import json
 from datetime import datetime
-import json
+from urllib.request import urlopen
+from github_commits import get_commits 
+import sqlite3
+                                                                                                                                       
+app = Flask(__name__)                                                                                                                  
 
-app = Flask(__name__)
+# Route pour afficher la page avec le graphique
+@app.route('/commits/')
+def show_commits():
+    data = get_commits()
+    return render_template('commits.html', chart_data=data)
 
 @app.route('/')
 def hello_world():
-    return render_template('hello.html')  # Comm2
+    return render_template('hello.html')
+  #test comit
+
+@app.route('/histogramme/')
+def histogramme():
+    return render_template('histogramme.html')
 
 @app.route("/contact/")
-def moncontact():
-    return render_template("contact.html")
+def MaPremiereAPI():
+    return render_template('contact.html')
 
 @app.route('/tawarano/')
 def meteo():
@@ -21,34 +35,13 @@ def meteo():
     results = []
     for list_element in json_content.get('list', []):
         dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('main', {}).get('temp') - 273.15  # Conversion de Kelvin en °C
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
 
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
-
-@app.route("/histogramme/")
-def mongraphique2():
-    return render_template("graphique2.html")
-
-@app.route('/commits/')
-def commits():
-    # URL de l'API GitHub pour les commits
-    url = 'https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits'
-    response = urlopen(url)
-    commits_data = json.loads(response.read().decode('utf-8'))
-    
-    # Préparer les données pour l'affichage
-    results = []
-    for commit in commits_data:
-        commit_date = commit['commit']['author']['date']
-        # Convertir la date ISO 8601 en timestamp Unix
-        dt_value = datetime.strptime(commit_date, '%Y-%m-%dT%H:%M:%SZ').timestamp()
-        results.append({'Jour': dt_value, 'temp': 1})  # Utiliser 'temp': 1 pour simplifier l'exemple
-
-    return jsonify(results=results)
-
+  
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
